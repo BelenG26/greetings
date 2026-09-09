1d72837d7270b965d8cce708df627fd03c5c3323
Belen Aguilar <bg.belen.aguilar@gmail.com>
2026-09-09 17:41:48 -0500

    Add banner Welcome message at start of program

---
+++ camel.py
@@ -0,0 +1,23 @@
+def camelcase(sentence):
+    #convert sentence to camelCase
+    title_case = sentence.title()#uppercase first letter of each word
+    upper_camel_cased = title_case.replace(' ','') # remove spaces
+    #lowercase first letter, join with rest of string
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
+
+def banner():
+    """ Display program name """
+    message = 'Awesome camelcase program!'
+    stars = '*' * len(message)
+    print(f'n{stars}\n{message}\n{stars}\n')
+          
+
+
+def main():
+    banner()
+    sentence = input('Enter your sentence: ')
+    output = camelcase(sentence)
+    print(output)
+
+if __name__ == '__main__':
+    main()
